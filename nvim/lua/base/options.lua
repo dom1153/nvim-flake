f56f3391a1fccbbf2cff689e4ceb06bfd3082ffd
Dominic Dang <dominicdang@protonmail.com>
2024-04-19 21:35:40 -0500

    reorg options

--- nvim/lua/base/options.lua
+++ nvim/lua/base/options.lua
@@ -1,106 +1,108 @@
--- local cmd = vim.cmd
-local g = vim.g
-local fn = vim.fn
-local opt = vim.o
-
 --[[ VIM GLOBALS ]]
 
 -- See `:help mapleader`
-g.mapleader = ' '
-g.maplocalleader = ' '
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 --[[ VIM OPTIONS ]]
 
 -- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
-opt.number = true
-opt.relativenumber = true
+vim.o.number = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-opt.mouse = 'a'
+vim.o.mouse = 'a'
 
 -- Don't show the mode, since it's already in status line
-opt.showmode = false
+vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 -- Remove this option if you want your OS clipboard to remain independent.
 -- See `:help 'clipboard'`
-opt.clipboard = 'unnamedplus'
+vim.o.clipboard = 'unnamedplus'
 
 -- indent wrapped lines
-opt.breakindent = true
+vim.o.breakindent = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-opt.ignorecase = true
-opt.smartcase = true
-opt.grepprg = 'rg --vimgrep'
-opt.grepformat = '%f:%l:%c:%m'
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.grepprg = 'rg --vimgrep'
+vim.o.grepformat = '%f:%l:%c:%m'
 
 -- Keep signcolumn on by default, prevents the screen from jumping
-opt.signcolumn = 'yes'
+vim.o.signcolumn = 'yes'
 
 -- Decrease update time
-opt.updatetime = 50 -- 4000ms by default, try 50, 250
-opt.timeoutlen = 10 -- (used for which-key)
+vim.o.updatetime = 50 -- 4000ms by default, try 50, 250
+vim.o.timeoutlen = 10 -- (used for which-key)
 
 -- Set completeopt to have a better completion experience
-opt.completeopt = 'menuone,noselect,noinsert' -- mostly just for cmp
+vim.o.completeopt = 'menuone,noselect,noinsert' -- mostly just for cmp
 
 -- Enable persistent undo history
-opt.swapfile = false
-opt.backup = false
-opt.undofile = true
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
 
 -- Configure how new splits should be opened
-opt.splitright = true
-opt.splitbelow = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 -- Sets how neovim will display certain whitespace in the editor.
 --   See `:help 'list'`
 --   and `:help 'listchars'`
-opt.list = true
-opt.listchars = 'tab:» ,trail:-,nbsp:␣' -- eol:↲
+vim.o.list = true
+vim.o.listchars = 'tab:» ,trail:-,nbsp:␣' -- eol:↲
 
 -- Preview substitutions live, as you type!
-opt.inccommand = 'split'
+vim.o.inccommand = 'split'
 
 -- Show which line your cursor is on
-opt.cursorline = true
+vim.o.cursorline = true
 
 -- Set fold settings
 -- These options were reccommended by nvim-ufo
 -- See: https://github.com/kevinhwang91/nvim-ufo--minimal-configuration
-opt.foldcolumn = '0'
-opt.foldlevel = 99
-opt.foldlevelstart = 99
-opt.foldenable = true
+vim.o.foldcolumn = '0'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-opt.scrolloff = 8
+vim.o.scrolloff = 8
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mod
-opt.hlsearch = true
+vim.o.hlsearch = true
 
 -- Enables 24-bit RGB color in the |TUI|.
-if fn.has('termguicolors') then
-  opt.termguicolors = true
+if vim.fn.has('termguicolors') then
+  vim.o.termguicolors = true
 end
 
 -- Set encoding type
-opt.encoding = 'utf-8'
-opt.fileencoding = 'utf-8'
+vim.o.encoding = 'utf-8'
+vim.o.fileencoding = 'utf-8'
 
 -- More space in the neovim command line for displaying messages
-opt.cmdheight = 2
+vim.o.cmdheight = 2
 
 -- Maximum number of items to show in the popup menu (0 means "use available screen space")
-opt.pumheight = 0
+vim.o.pumheight = 0
 
 -- save before executing certain commands
-opt.autowrite = true
+vim.o.autowrite = true
 
 -- Use conform-nvim for gq formatting. ('formatexpr' is set to vim.lsp.formatexpr(), so you can format lines via gq if the language server supports it)
-opt.formatexpr = "v:lua.require'conform'.formatexpr()"
+vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 
 -- disable text wrap by default
-opt.wrap = false
+vim.o.wrap = false
+
+-- some autocmd or startup task is breaking shortmess
+-- we don't need it anyways
+vim.opt.shortmess:append('I')
+
+-- keeps traditional vim g/GG
+vim.o.startofline = true
